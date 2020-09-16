827de293c9a8119b07f1935db761d3262b6266c2
Daehee Park <hello@daehee.com>
2020-09-16 12:19:19 -0700

    exclude IPv6 ranges for Azure

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ cat data/aws.json | jq -r '.prefixes[] | select(.region=="us-gov-east-1" or .reg
 # Extract dynamic download URL: https://stackoverflow.com/a/53939113
 AZURE_RANGES_URL=$(curl -Lfs 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=57063' | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep "download.microsoft.com/download/" | grep -m 1 -Eo '(http|https)://[^"]+')
 wget -O data/azure.json $AZURE_RANGES_URL
-cat data/azure.json | jq -r '.values[] | .properties.addressPrefixes[]' | sort -u > ranges-azure.txt
+cat data/azure.json | jq -r '.values[] | .properties.addressPrefixes[]' | grep -oP '^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$' | sort -u > ranges-azure.txt
 # for labels containing "DoD" only
-cat data/azure.json | jq -r '.values[] | select(.name | test("dod"; "i")) | .properties.addressPrefixes[]' | sort -u > ranges-azure-dod.txt
+cat data/azure.json | jq -r '.values[] | select(.name | test("dod"; "i")) | .properties.addressPrefixes[]' | grep -oP '^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$' | sort -u > ranges-azure-dod.txt
