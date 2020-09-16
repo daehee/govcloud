a5156f378de6902dd99c59ab4f828bddc949e304
Daehee Park <hello@daehee.com>
2020-09-16 12:06:21 -0700

    Handle dynamic download URL for Azure json

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@ cat data/aws.json | jq -r '.prefixes[] | select(.region=="us-gov-east-1" or .reg
 
 # Get Microsoft Azure IP ranges
 # Download link: https://www.microsoft.com/en-us/download/confirmation.aspx?id=57063
-wget -O data/azure.json https://download.microsoft.com/download/6/4/D/64DB03BF-895B-4173-A8B1-BA4AD5D4DF22/ServiceTags_AzureGovernment_20200824.json
+# Extract dynamic download URL: https://stackoverflow.com/a/53939113
+AZURE_RANGES_URL=$(curl -Lfs 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=57063' | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep "download.microsoft.com/download/" | grep -m 1 -Eo '(http|https)://[^"]+')
+wget -O data/azure.json $AZURE_RANGES_URL
 cat data/azure.json | jq -r '.values[] | .properties.addressPrefixes[]' | sort -u > ranges-azure.txt
 # for labels containing "DoD" only
 cat data/azure.json | jq -r '.values[] | select(.name | test("dod"; "i")) | .properties.addressPrefixes[]' | sort -u > ranges-azure-dod.txt
